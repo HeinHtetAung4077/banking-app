68a0b174f262c9a496a5ea6456f3050e408fa632
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-08-05 18:14:59 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,24 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { first_name : 'Adrian', lastName : 'Joe' }
   return (
-    <main>
-      SIDEBAR
+    <main className="flex w-full h-screen font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="Logo" width={30} height={30} />
+          <MobileNav user={loggedIn} />
+        </div>
       {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Adrian', lastName: 'Joe', email: 'heinhtet@gmail.com' }
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext="Access and manage your account and transaction efficiently."
+            />
 
-export default Home
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.30}
+            />
+        </header>
+        Recent Transactions 
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance : 125.30}, {currentBalance : 300.50}]} />
+    </section>
+  );
+};
+
+export default Home;
