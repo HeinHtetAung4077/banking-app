8afab5c890181e6d80e98b15d2e908950dd6eddb
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-08-09 14:37:40 +0530

    TSS

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,19 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { first_name : 'Adrian', lastName : 'Joe' }
+  // const loggedIn = { first_name : 'Adrian', lastName : 'Joe' }
+  const loggedIn = await getLoggedInUser();
+  // if (!loggedIn) return redirect("/sign-in");
+  console.log(loggedIn);
+  
   return (
     <main className="flex w-full h-screen font-inter">
       <Sidebar user={loggedIn} />
@@ -17,7 +23,7 @@ export default function RootLayout({
           <Image src="/icons/logo.svg" alt="Logo" width={30} height={30} />
           <MobileNav user={loggedIn} />
         </div>
-      {children}
+        {children}
       </div>
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-    const loggedIn = { firstName: 'Adrian', lastName: 'Joe', email: 'heinhtet@gmail.com' }
+const Home = async () => {
+    // const loggedIn = { firstName: 'Adrian', lastName: 'Joe', email: 'heinhtet@gmail.com' }
+    const loggedIn = await getLoggedInUser()
+    
   return (
     <section className="home">
       <div className="home-content">
@@ -12,7 +15,7 @@ const Home = () => {
             <HeaderBox 
                 type="greeting"
                 title="Welcome"
-                user={loggedIn?.firstName || "Guest"}
+                user={loggedIn?.name || "Guest"}
                 subtext="Access and manage your account and transaction efficiently."
             />
 
